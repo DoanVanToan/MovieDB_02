0eda31010c74d9b1c59b3ffe3ca854a1b765f723
haiminhtran810 <haiminhtran810@gmail.com>
2017-11-27 15:00:11 +0700

    Giau KEY API

--- app/src/main/java/com/example/hcm_102_0006/android_project_m/data/source/remote/MovieApi.java
+++ app/src/main/java/com/example/hcm_102_0006/android_project_m/data/source/remote/MovieApi.java
@@ -1,11 +1,10 @@
 package com.example.hcm_102_0006.android_project_m.data.source.remote;
 
-
+import com.example.hcm_102_0006.android_project_m.BuildConfig;
 import com.example.hcm_102_0006.android_project_m.data.model.GenreResponse;
 import com.example.hcm_102_0006.android_project_m.data.model.Movie;
 import com.example.hcm_102_0006.android_project_m.data.model.MovieDetail;
 import com.example.hcm_102_0006.android_project_m.service.model.ResultResponse;
-import com.example.hcm_102_0006.android_project_m.service.repository.DeveloperKey;
 
 
 import retrofit.http.GET;
@@ -20,29 +19,29 @@ public interface MovieApi {
     String SERVICE_URL = "https://api.themoviedb.org/3";
 
     //Get information category: https://api.themoviedb.org/3/movie/upcoming?api_key=c733ac6aba3d86364a56d1145bc1d1f9
-    @GET("/movie/{category}?api_key=" + DeveloperKey.KEY_MOVIE_API)
+    @GET("/movie/{category}?api_key=" + BuildConfig.MOVIE_KEY)
     Observable<ResultResponse> getMovie(@Path("category") String category);
 
     //https://api.themoviedb.org/3/genre/movie/list?api_key=c733ac6aba3d86364a56d1145bc1d1f9&language=en-US
-    @GET("/genre/movie/list?api_key=" + DeveloperKey.KEY_MOVIE_API)
+    @GET("/genre/movie/list?api_key=" + BuildConfig.MOVIE_KEY)
     Observable<GenreResponse> getGenres();
 
     // get movie genres
-    @GET("/genre/{genre_id}/movies?api_key=" + DeveloperKey.KEY_MOVIE_API + "&language=en-US&include_adult=false&sort_by=created_at.asc")
+    @GET("/genre/{genre_id}/movies?api_key=" + BuildConfig.MOVIE_KEY + "&language=en-US&include_adult=false&sort_by=created_at.asc")
     Observable<ResultResponse> getMovieGenres(@Path("genre_id") String genreId);
 
     // get movie detail /movie/{movie_id}
-    @GET("/movie/{movie_id}?api_key=" + DeveloperKey.KEY_MOVIE_API+"&append_to_response=videos")
+    @GET("/movie/{movie_id}?api_key=" + BuildConfig.MOVIE_KEY+"&append_to_response=videos")
     Observable<MovieDetail> getMovieDetail(@Path("movie_id") String movieId);
 
     // Get videos
     //get /movie/{movie_id}/videos
-    @GET("/movie/{movie_id}/videos?api_key=" + DeveloperKey.KEY_MOVIE_API)
+    @GET("/movie/{movie_id}/videos?api_key=" + BuildConfig.MOVIE_KEY)
     Observable<MovieDetail> getMovieVideo(@Path("movie_id") String movieId);
     // then get key movie. https://www.youtube.com/watch?v=SUXWAEX2jlg
 
     // Get all movie of company https://api.themoviedb.org/3/company/{company_id}/movies?api_key=c733ac6aba3d86364a56d1145bc1d1f9&language=en-US
     // /company/{company_id}/movies
-    @GET("/company/{company_id}/movies?api_key=" + DeveloperKey.KEY_MOVIE_API + "&language=en-US")
+    @GET("/company/{company_id}/movies?api_key=" + BuildConfig.MOVIE_KEY + "&language=en-US")
     Observable<Movie> getMovieCompany(@Path("company_id") String companyId);
 }
--- app/src/main/java/com/example/hcm_102_0006/android_project_m/service/repository/DeveloperKey.java
+++ app/src/main/java/com/example/hcm_102_0006/android_project_m/service/repository/DeveloperKey.java
@@ -6,6 +6,4 @@ package com.example.hcm_102_0006.android_project_m.service.repository;
 
 public class DeveloperKey {
     public static final String DEVELOP_KEY = "AIzaSyADDdmasYNumHvYxldv28AX6gkw0Yg4wJM";
-    public static final String SERVICE_URL = "https://api.themoviedb.org/3";
-    public static final String KEY_MOVIE_API = "c733ac6aba3d86364a56d1145bc1d1f9";
 }
